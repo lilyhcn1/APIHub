60fb5f2a32cb4c1961dcd573a5202775da29a138
lilyhcn1 <lilyhcn1@upsir.com>
2024-03-15 16:14:17 +0800

    11

--- soft/nircmd启动uvicorn.bat
+++
@@ -1,2 +0,0 @@
-@set nowpath=%cd%
-"%nowpath%\soft\nircmd.exe" exec hide cmd /c soft\Æô¶¯uvicorn.bat
--- soft/启动uvicorn.bat
+++
@@ -1,3 +0,0 @@
-@set nowpath=%cd%
-@cd py
-"%nowpath%\soft\python38\python38.exe" -m uvicorn wsmain:app --host 0.0.0.0 --port 5034 --reload
